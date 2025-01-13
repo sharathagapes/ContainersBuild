12c6fd104e61ac306214b67d85b542ce163fb1dc
bismaya89 <131163218+bismaya89@users.noreply.github.com>
2025-01-13 19:36:06 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 export EULA=Y  # Set the EULA environment variable
 /opt/mssql/bin/sqlservr &  # Start SQL Server in the background
 sleep 50  # Wait for SQL Server to start
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Your!Strong@Passw0rd -d master -i /usr/src/app/init.sql -C
+#/opt/mssql-tools18/bin/sqlcm -S localhost -U sa -P Your!Strong@Passw0rd -d master -i /usr/src/app/init.sql -C
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Your!Strong@Passw0rd -d master -i /usr/src/app/init.sql -C
 wait  # Wait for SQL Server to finish
